3abcbdcce26296a0d23f362a6c7d384909c85c0a
pablococciaglia <50939772+pablococciaglia@users.noreply.github.com>
2021-06-05 14:39:25 +0200

    cambio de colores
    cambio de colores en redsoc
    cambio de orden en el navbar
    agregado de icono de whatsapp con svg
    pendiente: la alineacion de los numeros de telefono

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,8 @@ export const Navbar = () => {
                 <a href="#!" className="brand-logo navbar__container"><img src={logoM} alt="Monisu icon"/></a>
                 <a href="#!" data-target="mobile-demo" className="sidenav-trigger" ><i className="material-icons">menu</i></a>
                 <ul className="right hide-on-med-and-down navbar__container">
-                    <li><a href="#!" className=" red-text text-accent-4">Estilos</a></li>
                     <li><a href="#!" className=" red-text text-accent-4">Mapa</a></li>
+                    <li><a href="#!" className=" red-text text-accent-4">Estilos</a></li>
                     <li><a href="#!" className=" red-text text-accent-4">Productos</a></li>
                     <li><a href="#!" className=" red-text text-accent-4">Nosotros</a></li>
                     <li><a href="#!" className=" red-text text-accent-4">Contacto</a></li>
@@ -22,8 +22,8 @@ export const Navbar = () => {
             </nav>
 
             <ul className="sidenav navbar__container grey lighten-3 " id="mobile-demo">
-                <li><a href="#!" className=" red-text text-accent-4">Estilos</a></li>
                 <li><a href="#!" className=" red-text text-accent-4">Mapa</a></li>
+                <li><a href="#!" className=" red-text text-accent-4">Estilos</a></li>
                 <li><a href="#!" className=" red-text text-accent-4">Productos</a></li>
                 <li><a href="#!" className=" red-text text-accent-4">Nosotros</a></li>
                 <li><a href="#!" className=" red-text text-accent-4">Contacto</a></li>
--- src/components/RedSoc.js
+++ src/components/RedSoc.js
@@ -1,46 +1,55 @@
 import React from 'react'
 import logoF from '../assets/logos/facebook.svg'
 import logoI from '../assets/logos/instagram.svg'
+import logoW from '../assets/logos/whatsapp.svg'
 export const RedSoc = () => {
     return (
         <div className="row">
-            <div className="col s12 m6" white>
-                <div className="redsoc__container white">
+            <div className="col s12 m6">
+                <div className="redsoc__container">
                     <div>
                         <span className="redsoc__title">Monisú</span>
                         <span className="redsoc__subtitle">Marbell</span>
                     </div>
-                    <div>
-                        <a href="https://www.facebook.com/monisuhairandbeauty" target="_blank" className="redsoc__icon"><img src={logoF} alt="Facebook icon" width="40px"/></a>
-                        <a href="https://www.instagram.com/monisu.marbella/" target="_blank" className="redsoc__icon"><img src={logoI} alt="Instagram icon" width="40px"/></a>
-                    </div>
-                    <div>
-                        <i class="material-icons grey-text">phone</i>
-                        <a href="tel:952900650" target="_blank" >952900650</a>
-                    </div>
-                    <div>
-                        
-                        <a href="https://wa.me/34647015846" target="_blank" >(+34) 647015846</a>
+                    <div className="redsoc__containerlinks">
+                        <div>
+                            <a href="https://www.facebook.com/monisuhairandbeauty" rel="noreferrer" target="_blank" className="redsoc__icon"><img src={logoF} alt="Facebook icon" width="40px"/></a>
+                        </div>
+                        <div>
+                            <a href="https://www.instagram.com/monisu.marbella/" rel="noreferrer" target="_blank" className="redsoc__icon"><img src={logoI} alt="Instagram icon" width="40px"/></a>
+                        </div>
+                        <div>
+                            <a href="tel:952900650" rel="noreferrer" target="_blank"><i className="material-icons black-text redsoc__icon">phone</i></a>
+                            <a href="tel:952900650" rel="noreferrer" target="_blank" className="redsoc__icontext">952900650</a>
+                        </div>
+                        <div>
+                            <a href="https://wa.me/34647015846" rel="noreferrer" target="_blank"><img src={logoW} alt="Facebook icon" width="40px" className="redsoc__icon"/></a>
+                            <a href="https://wa.me/34647015846" rel="noreferrer" target="_blank" className="redsoc__icontext">(+34)&nbsp;647015846</a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="col s12 m6" white>
-                <div className="redsoc__container white">
+            <div className="col s12 m6">
+                <div className="redsoc__container">
                     <div>
                         <span className="redsoc__title" target="_blank" >Monisú</span>
                         <span className="redsoc__subtitle" target="_blank" >Fuengirol</span>
                     </div>
-                    <div>
-                        <a href="https://www.facebook.com/monisu.fuengirola" target="_blank" className="redsoc__icon"><img src={logoF} alt="Facebook icon" width="40px"/></a>
-                        <a href="https://www.instagram.com/monisu.fuengirola/" target="_blank" className="redsoc__icon"><img src={logoI} alt="Instagram icon" width="40px"/></a>
-                    </div>
-                    <div>
-                        <i class="material-icons grey-text">phone</i>
-                        <a href="tel:952473202">952473202</a>
-                    </div>
-                    <div>
-                        
-                        <a href="https://wa.me/34666341769">(+34) 666341769</a>
+                    <div className="redsoc__containerlinks">
+                        <div>
+                            <a href="https://www.facebook.com/monisu.fuengirola" rel="noreferrer" target="_blank" className="redsoc__icon"><img src={logoF} alt="Facebook icon" width="40px"/></a>
+                        </div>
+                        <div>
+                            <a href="https://www.instagram.com/monisu.fuengirola/" rel="noreferrer" target="_blank" className="redsoc__icon"><img src={logoI} alt="Instagram icon" width="40px"/></a>
+                        </div>
+                        <div>
+                            <a href="tel:952473202" rel="noreferrer" ><i className="material-icons black-text redsoc__icon">phone</i></a>&nbsp;
+                            <a href="tel:952473202" rel="noreferrer" className="redsoc__icontext">952473202</a>
+                        </div>
+                        <div>
+                            <a href="https://wa.me/34666341769" rel="noreferrer" target="_blank"><img src={logoW} alt="Facebook icon" width="40px" className="redsoc__icon"/></a>
+                            <a href="https://wa.me/34666341769" rel="noreferrer" target="_blank" className="redsoc__icontext">(+34)&nbsp;666341769</a>
+                        </div>
                     </div>
                 </div>
             </div>
