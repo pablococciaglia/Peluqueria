759e426ca597da3fadc2a7f6379fd57542da4c6b
pablococciaglia <50939772+pablococciaglia@users.noreply.github.com>
2021-06-15 00:05:06 +0200

    Update Maps.js

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -74,7 +74,7 @@ export const Maps = () => {
                         </div>
                         
                         <WrappedMap
-                            googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCr4tBC5_Pwlw7NSTqmJALo1fE4o6Ne_z4`}
+                            googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key= use su propia api key de maps`}
                             loadingElement ={<div style={{height: "100%"}} />}
                             containerElement ={<div style={{height: "100%"}} />}
                             mapElement ={<div style={{height: "100%"}} className="maps__container" />}
@@ -90,7 +90,7 @@ export const Maps = () => {
                         </div>
 
                         <WrappedMap2
-                            googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCr4tBC5_Pwlw7NSTqmJALo1fE4o6Ne_z4`}
+                            googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key= use su propia api key de maps`}
                             loadingElement ={<div style={{height: "100%"}} />}
                             containerElement ={<div style={{height: "100%"}} />}
                             mapElement ={<div style={{height: "100%"}} className="maps__container" />}
